141c2adef54ee38986c8aa44bf9908d15e3acefa
christian ardeña <christian30199@gmail.com>
2023-10-24 18:11:37 +0800

    'additional'

---
+++ TESDA-activity/cwd-project-2023/script.js
@@ -0,0 +1,51 @@
+
+$(document).ready(function(){
+$('.materialboxed').materialbox();
+});
+
+$(document).ready(function(){
+  $('.slider').slider({
+    height:600
+  });
+});
+
+
+window.onload = function () {
+
+  var chart1 = new CanvasJS.Chart("chartContainer", {
+    animationEnabled: true,
+    theme: "light2",
+    title:{
+      text: "Simple Line Chart"
+    },
+    data: [{        
+      type: "line",
+          indexLabelFontSize: 16,
+      dataPoints: [
+        { y: 450 },
+        { y: 414},
+        { y: 520, indexLabel: "\u2191 highest",markerColor: "red", markerType: "triangle" },
+        { y: 460 },
+        { y: 450 },
+        { y: 500 },
+        { y: 480 },
+        { y: 480 },
+        { y: 410 , indexLabel: "\u2193 lowest",markerColor: "DarkSlateGrey", markerType: "cross" },
+        { y: 500 },
+        { y: 480 },
+        { y: 510 }
+      ]
+    }]
+  });
+  chart1.render();
+}
+
+
+$(document).ready(function(){
+  $('.sidenav').sidenav();
+});
+
+$(document).ready(function(){
+  $('.collapsible').collapsible();
+});
+
